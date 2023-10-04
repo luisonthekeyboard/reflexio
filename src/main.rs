f3e07682ccd74a2958f5a745e1a147c40ff5dbba
Luís <luis@luisonthekeyboard.com>
2023-10-04 08:54:29 +0200

    Base echo version.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input text
+    #[arg(required(true), num_args(1..))]
+    text: Vec<String>,
+
+    /// Skip newline at the end
+    #[arg(short, default_value_t = false)]
+    newline_skip: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let ending = if args.newline_skip { "" } else { "\n" };
+    let input_text = args.text.join(" ");
+
+    print!("{}{}", input_text, ending);
 }
